382e20fa977d445333ea123456d2b662576cec3b
D.Navickas <112947625+DovydasNavickas0@users.noreply.github.com>
2022-11-27 20:44:54 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -4,14 +4,17 @@ import data from './data.js';
 //console.log(data);
 //console.log(result[0])
 
-function createrCatalogue(){ 
+function checkCatalogue(){ 
 
     if(localStorage.getItem('favorite_movies') !== null){
         console.log("It already exists")
 
     }else{
-        localStorage.setItem('favorite_movies')
+        console.log("it doesn't exist")
+        return
     }
+
+    console.log("createrCatalogue done")
 }
 
 function search_data(x){
@@ -19,36 +22,92 @@ function search_data(x){
     return favorite_movie_find
 }
 
-function addmovie(x){
+function addmovie(x){ //doesn't check if movie has already been added
 
-    let favorite_movie_add = data.find(movie => movie.id === x)
+    let favorite_movie_add = search_data(x)
 
-    new_movie = {
-        movieId: favorite_movie_add.id,
-        movieTitle: favorite_movie_add.title
+    //console.log(search_data(x))
+
+    let new_movie = {
+        "movieId": favorite_movie_add.id,
+        "movieTitle": favorite_movie_add.title
     }
 
-    const favorite_movie = (() => {
+    let movie_id = favorite_movie_add.id;
+
+    let ID = x; 
+
+//    console.log(movie_id)
+//    console.log(new_movie)
+
+    const movies = (() => {
         const items = localStorage.getItem('favorite_movies');
         return items === null
             ? []
             : JSON.parse(items);
     })();
 
-    let movies = [];
+    let IDs = [];
 
-    for(let i = 0; i< favorite_movie.length; i++){
-        movies.push(favorite_movie[i].new_movie)
+    for(let i = 0; i < movies.length; i++){
+        IDs.push(movies[i].movie_id);
+    }
+
+//    console.log(IDs)
+    if(IDs.includes(ID.value)){
+        alert("Preke jau yra");
+    }else{
+//        console.log("nera")
+        movies.push(new_movie);
     }
 
     localStorage.setItem('favorite_movies', JSON.stringify(movies));
 
+    console.log("addmovie done")
 }
 
-function search_storage(x){
+function search_storage(){
     
+    let found_movie = JSON.parse(localStorage.getItem('favorite_movies'));
+
+//    console.log(found_movie)
+
+    let id_numbers = found_movie.map(obj => parseInt(obj.movieId))
+
+//    console.log(id_numbers)
+
+    for(let i=0; i<id_numbers.length; i++){
+        let movie_print = data.find(movie => movie.id === id_numbers[i])
+        console.log(movie_print)
+    }
+
 }
 
-//console.log(createrCatalogue())
+function delete_movie(x){
+
+    let found_movie = JSON.parse(localStorage.getItem('favorite_movies'));
+
+//    console.log(found_movie)
+
+    let target = found_movie.findIndex(found_movie => found_movie.movieId === x)
+
+    if (target > -1) {
+        found_movie.splice(target, 1); 
+    }
+
+//    console.log(found_movie)
+
+    localStorage.setItem('favorite_movies', JSON.stringify(found_movie))
+
+    console.log('movie deleted')
+
+}
+
+//checkCatalogue()
 //console.log(search_data(436270))
-//console.log(createrFavourite(436270))
+//addmovie(436270)
+//addmovie(829280)
+//addmovie(760161)
+//search_storage();
+//delete_movie(436270);
+
